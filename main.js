92ecccd78be001d9259f3ec1a9d7e1599c44b246
Karan Gupta <68427055+karan10i@users.noreply.github.com>
2023-11-05 22:57:04 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+document.getElementsById("intro").onclick = function () {
+    // Define the URL of the page you want to navigate to
+    var destinationPage = "intro.html";
+    
+    // Redirect to the destination page
+    window.location.href = destinationPage;
+};
